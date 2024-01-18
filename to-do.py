fe43a3f297343e5426d2dc702c705fe6f06c3ef2
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-01-18 13:27:03 +0530

    Updated with Add, Show and Exit

--- to-do.py
+++ to-do.py
@@ -1,8 +1,12 @@
-prompt="Enter a To-Do item: "
-
 todos=[]
-
 while True:
-    todo=input(prompt)
-    todos.append(todo)
-    print("To-Do list:", todos)
+    action = input("Add or Show or Exit ? ")
+
+    match action:
+        case "Add":
+            todo = input("Enter a to-do item: ")
+            todos.append(todo)
+        case "Show":
+            print("To-Do list:", todos)
+        case "Exit":
+            break
