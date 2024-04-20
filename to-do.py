779b9fd76339abac7a3655242efa1729d7204a78
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-04-20 10:23:44 +0530

    If added insted of case

--- to-do.py
+++ to-do.py
@@ -1,76 +1,58 @@
 while True:
     action = input("Add or Show or Edit or Complete or Exit? ")
-    action=action.strip()
+    action = action.strip()
 
-    match action:
-        case "Add":
-            todo = input("Enter a to-do item: ") + "\n"
+    if "Add" in action:
+        todo = action[4:]
 
-            '''file=open("todos.txt","r")       To read file and append the new item
-            todos=file.readlines()
-            file.close()'''
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open("todos.txt","r") as file:             #To read file but don't need to close the file
-                todos = file.readlines()
+        todos.append(todo + "\n")
 
-            todos.append(todo)
-            
-            '''file=open("todos.txt","w")  
+        with open("todos.txt", "w") as file:
             file.writelines(todos)
-            file.close()'''
+        print(f"{todo} is added!")
 
-            with open("todos.txt","w") as file:
-                file.writelines(todos)             
-        
-        case "Show":
+    elif "Show" in action:
 
-            with open("todos.txt","r") as file:             #To read file but don't need to close the file
-                todos = file.readlines()
-            
-            ''' To remove the \n from the list using for loop
-            new_todos = []
-            for item in todos:
-                new_item = item.strip('\n')
-                new_todos.append(new_item)
-            '''
-            #new_todos = [item.strip('\n') for item in todos] # List comprehension to remove the \n from the list
-            
-            for index, items in enumerate(todos):
-                items = items.strip('\n')
-                row = f"{index+1}-{items}"
-                print(row)
-        
-        case "Edit":
-            number = int(input("Enter the number of the item to edit: "))
-            number=number-1
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open("todos.txt","r") as file:             #To read file but don't need to close the file
-                todos = file.readlines()
+        for index, item in enumerate(todos, 1):
+            print(f"{index}-{item.strip()}")
 
-            new_todo = input("Enter the new to-do item: ")
-            todos[number] = new_todo+"\n"
+    elif "Edit" in action:
+        number = int(input("Enter the number of the item to edit: "))
+        number -= 1
 
-            with open("todos.txt","w") as file:
-                file.writelines(todos) 
-        
-        case "Complete":
-            number = int(input("Enter the number of the item to complete: "))
-            number=number-1
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open("todos.txt","r") as file:             #To read file but don't need to close the file
-                todos = file.readlines()
+        new_todo = input("Enter the new to-do item: ")
+        todos[number] = new_todo + "\n"
 
-            todo_to_complete = todos[number]
-            todos.pop(number)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-            with open("todos.txt","w") as file:
-                file.writelines(todos)
+    elif "Complete" in action:
+        number = int(input("Enter the number of the item to complete: "))
+        number -= 1
 
-            message = f"{todo_to_complete.strip()} is completed!"
-            print(message)
-        
-        case "Exit":
-            break
-        
-        case _:
-            print("Invalid action")
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+
+        todo_to_complete = todos[number]
+        todos.pop(number)
+
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+
+        message = f"{todo_to_complete.strip()} is completed!"
+        print(message)
+
+    elif "Exit" in action:
+        break
+
+    else:
+        print("Invalid action")
