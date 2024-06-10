d4e2c12fb6dc11a90b5b0145604691f219677c90
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-06-10 23:50:12 +0530

    Doc string added

--- to-do.py
+++ to-do.py
@@ -1,9 +1,15 @@
 def get_todos(filepath="todos.txt"):
+    """Read the to-do list from the file 
+    and return it as a list"""
+
     with open(filepath, "r") as file_local:
          todos_local = file_local.readlines()
          return todos_local
 
 def write_todos(todos_arg,filepath="todos.txt"):
+
+    """Write the to-do list to the file"""
+
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
