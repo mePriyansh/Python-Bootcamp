ec4835788c5a74c2d65c0a6d9daa87be63ae0cb4
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-06-13 15:58:13 +0530

    Module added

--- to-do.py
+++ to-do.py
@@ -1,17 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """Read the to-do list from the file 
-    and return it as a list"""
-
-    with open(filepath, "r") as file_local:
-         todos_local = file_local.readlines()
-         return todos_local
-
-def write_todos(todos_arg,filepath="todos.txt"):
-
-    """Write the to-do list to the file"""
-
-    with open(filepath, "w") as file:
-        file.writelines(todos_arg)
+from functions import get_todos, write_todos
 
 while True:
     action = input("Add or Show or Edit or Complete or Exit? ")
