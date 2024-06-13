11e4fa1e2dd58a6e3a5f4a8b2c448b2624a31f13
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-06-13 08:46:55 +0530

    Decoupling added

--- bonus12.py
+++ bonus12.py
@@ -1,15 +1,22 @@
 feet_inches= input("Enter the height in feet and inches: ")
 
-
-def convert(feet_inches):
+def parse(feet_inches):
     parts = feet_inches.split()
     feet = float(parts[0])
     inches = float(parts[1])
+    return {"feet":feet, "inches":inches}
+
+
+
+def convert(feet,inches):
 
     meters = (feet * 0.3048) + (inches * 0.0254)
     return meters
 
-result= convert(feet_inches)
+parsed = parse(feet_inches)
+result= convert(parsed['feet'],parsed['inches'])
+
+print(f"{parsed['feet']} feet {parsed['inches']} inches is equal to {result} meters")
 
 if result < 1.0:
     print("Not Possible")
