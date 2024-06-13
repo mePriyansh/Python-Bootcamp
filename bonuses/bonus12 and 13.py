5e1cb0836d477edfeff639a53a9e9ee2d1752ff8
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-06-13 17:20:22 +0530

    modules added for 2 functions

--- bonuses/bonus12 and 13.py
+++ bonuses/bonus12 and 13.py
@@ -1,17 +1,7 @@
+from paser14 import parse
+from convert14 import convert
+
 feet_inches= input("Enter the height in feet and inches: ")
-
-def parse(feet_inches):
-    parts = feet_inches.split()
-    feet = float(parts[0])
-    inches = float(parts[1])
-    return {"feet":feet, "inches":inches}
-
-
-
-def convert(feet,inches):
-
-    meters = (feet * 0.3048) + (inches * 0.0254)
-    return meters
 
 parsed = parse(feet_inches)
 result= convert(parsed['feet'],parsed['inches'])
