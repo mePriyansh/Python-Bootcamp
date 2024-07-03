9287334a394285d76687dee7991a8ecf49e55cd5
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-07-03 19:16:09 +0530

    compressor created

--- gui.py
+++ gui.py
@@ -3,8 +3,9 @@ import FreeSimpleGUI as sg
 
 label= sg.Text("Enter your task")
 input_box=sg.InputText(tooltip="Enter your task")
+add_button=sg.Button("Add",tooltip="Add your task")
 
-window=sg.Window("My To-Do App",layout=[[label,input_box]])
+window=sg.Window("My To-Do App",layout=[[label],[input_box,add_button]])
 window.read()
 window.close()
 
