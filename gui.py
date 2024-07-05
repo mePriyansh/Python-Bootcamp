1d24cee30b092135a23ed05c70b9994f61f7d276
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-07-05 13:35:34 +0530

    Add todo list

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import FreeSimpleGUI as sg
 label= sg.Text("Enter your task")
 input_box=sg.InputText(tooltip="Enter your task", key="todo")
 add_button=sg.Button("Add",tooltip="Add your task")
+listbox=sg.Listbox(values=functions.get_todos(), key='todos', 
+                   enable_events=True, size=[40,10])
+edit_button=sg.Button("Edit", tooltip="Edit your task")
 
 window=sg.Window("My To-Do App", 
-                 layout=[[label],[input_box,add_button]],
+                 layout=[[label],[input_box,add_button], [listbox, edit_button]],
                  font=("Helvetica", 15))
 
 while True:
@@ -19,6 +22,9 @@ while True:
             new_todo=values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
 
 window.close()
 
