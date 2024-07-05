912048b8fee904692722d4e41c299ec657c9116b
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-07-05 13:52:37 +0530

    Edit button added

--- gui.py
+++ gui.py
@@ -14,14 +14,28 @@ window=sg.Window("My To-Do App",
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos=functions.get_todos()
             new_todo=values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            
+        case "Edit":
+            todo_to_edit =values['todos'][0]
+            new_todo=values['todo']
+            todos=functions.get_todos()
+            index=todos.index(todo_to_edit)
+            todos[index]=new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])            
 
         case sg.WIN_CLOSED:
             break
