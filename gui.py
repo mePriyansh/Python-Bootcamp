97a51375a858062c1530508eba7c9a0fbac8c1f3
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-07-07 19:36:57 +0530

    Clock added, theme update and try-except block

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 from modules import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("Black")
+
+clock=sg.Text("",key="clock")
 label= sg.Text("Enter your task")
 input_box=sg.InputText(tooltip="Enter your task", key="todo")
 add_button=sg.Button("Add",tooltip="Add your task")
@@ -11,17 +15,17 @@ complete_button=sg.Button("Complete")
 exit_button=sg.Button("Exit")
 
 window=sg.Window("My To-Do App", 
-                 layout=[[label],
+                 layout=[[clock],
+                         [label],
                          [input_box,add_button], 
                          [listbox, edit_button, complete_button],
                          [exit_button]],
                  font=("Helvetica", 15))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos=functions.get_todos()
@@ -31,21 +35,27 @@ while True:
             window['todos'].update(values=todos)
             
         case "Edit":
-            todo_to_edit =values['todos'][0]
-            new_todo=values['todo']
-            todos=functions.get_todos()
-            index=todos.index(todo_to_edit)
-            todos[index]=new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)     
-                           #to update list during run-time
+            try:
+                todo_to_edit =values['todos'][0]
+                new_todo=values['todo']
+                todos=functions.get_todos()
+                index=todos.index(todo_to_edit)
+                todos[index]=new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)        #to update list during run-time
+            except IndexError:
+                sg.popup("No task selected", font=("Helvetica", 15))
+
         case "Complete":
-            todo_to_complete=values['todos'][0]
-            todos=functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)             #to update list during run-time
-            window['todo'].update(value="")  
+            try:
+                todo_to_complete=values['todos'][0]
+                todos=functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)             #to update list during run-time
+                window['todo'].update(value="")  
+            except IndexError:
+                sg.popup("No task selected", font=("Helvetica", 15))
             
         case "Exit":
             break                  
