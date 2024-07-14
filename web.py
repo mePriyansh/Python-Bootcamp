9ebd3c9803d5c7d4f4ed53271438927060c96ea2
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-07-14 23:13:38 +0530

    Add todo feature added

--- web.py
+++ web.py
@@ -3,10 +3,16 @@ from modules import functions
 
 todos=functions.get_todos()
 
+def add_todo():
+    todo=st.session_state["new_todo"]
+    todos.append(todo)
+    functions.write_todos(todos)
+
 st.title("My To-Do App")
 st.subheader("This is a simple To-Do App")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="",placeholder="Enter your task")
+st.text_input(label="",placeholder="Enter your task",
+              on_change=add_todo, key="new_todo")
