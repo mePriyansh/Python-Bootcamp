57fb6e18e1799bcc03d0954a118871582314c0ef
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-08-18 22:57:27 +0530

    Page added as per csv

--- Project 4- Pdf Template/main.py
+++ Project 4- Pdf Template/main.py
@@ -1,12 +1,16 @@
 from fpdf import FPDF
+import pandas as pd
+
+df=pd.read_csv(r"Project 4- Pdf Template\topics.csv")
 
 pdf=FPDF(orientation='P', unit='mm', format='A4')
 
-pdf.add_page()
-pdf.set_font("Times", size = 12, style='B')
-pdf.cell(w=0,h=12,txt="Hello World",
-         ln=1,align='L')
-pdf.set_font("Times", size = 10)
-pdf.cell(w=0,h=12,txt="Hi World",
-         ln=1,align='L')
+for index,row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font("Times", size = 24, style='B')
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0,h=12,txt=row["Topic"],
+            ln=1,align='L')
+    pdf.line(10,21,200,21)
+
 pdf.output('output.pdf')
