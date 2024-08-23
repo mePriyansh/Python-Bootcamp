68dc0c5ec9a0bc8fe2954a2ba9cff37ff3a342f3
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-08-23 21:16:09 +0530

    Add footer

--- Project 4- Pdf Template/main.py
+++ Project 4- Pdf Template/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 df=pd.read_csv(r"Project 4- Pdf Template\topics.csv")
 
 pdf=FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index,row in df.iterrows():
     pdf.add_page()
@@ -13,7 +14,17 @@ for index,row in df.iterrows():
             ln=1,align='L')
     pdf.line(10,21,200,21)
 
-    for i in range(row["Pages"]):
+    pdf.ln(260)
+    pdf.set_font("Times", size = 8, style='I')
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=10,txt=row['Topic'],align="R",)
+
+    for i in range(row["Pages"]-1):
         pdf.add_page()
+
+        pdf.ln(270)
+        pdf.set_font("Times", size = 8, style='I')
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0,h=10,txt=row['Topic'],align="R",)
 
 pdf.output('output.pdf')
