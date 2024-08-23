ee539ee443265c0227f48fdc6b01444bd73b5210
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-08-23 21:57:09 +0530

    Horizontal line added

--- Project 4- Pdf Template/main.py
+++ Project 4- Pdf Template/main.py
@@ -12,7 +12,10 @@ for index,row in df.iterrows():
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0,h=12,txt=row["Topic"],
             ln=1,align='L')
-    pdf.line(10,21,200,21)
+    #for line throughot page
+    for y in range(20,278,10):
+        pdf.line(10,y,200,y)
+    #pdf.line(10,21,200,21)
 
     pdf.ln(260)
     pdf.set_font("Times", size = 8, style='I')
@@ -21,10 +24,11 @@ for index,row in df.iterrows():
 
     for i in range(row["Pages"]-1):
         pdf.add_page()
-
         pdf.ln(270)
         pdf.set_font("Times", size = 8, style='I')
         pdf.set_text_color(180,180,180)
         pdf.cell(w=0,h=10,txt=row['Topic'],align="R",)
-
-pdf.output('output.pdf')
+            #for line throughot page
+        for y in range(20,278,10):
+            pdf.line(10,y,200,y)
+pdf.output('output_line.pdf')
