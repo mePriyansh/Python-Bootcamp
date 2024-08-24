34096d30b7d9992cb95bbde56940847acb20a02a
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-08-24 20:00:10 +0530

    Load excel file to data frame

---
+++ Project 5- Excel to pdf invoice/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import glob
+import os
+
+# Get the list of file paths
+filepaths = glob.glob("Project 5- Excel to pdf invoice\\Invoice\\*.xlsx")
+
+for filepath in filepaths:
+    df=pd.read_excel(filepath,sheet_name='Sheet 1')
+    print(df)
