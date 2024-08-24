9c17ad52e94d3d4ebd9489b829fb73a435b80658
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-08-24 20:40:31 +0530

    Minimal Pdf created

--- Project 5- Excel to pdf invoice/main.py
+++ Project 5- Excel to pdf invoice/main.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # Get the list of file paths
 filepaths = glob.glob("Project 5- Excel to pdf invoice\\Invoice\\*.xlsx")
 
 for filepath in filepaths:
     df=pd.read_excel(filepath,sheet_name='Sheet 1')
-    print(df)
+    pdf=FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    filename=Path(filepath).stem
+    invoice_nr=filename.split('-')[0]
+    pdf.set_font(family="Times",size=16,style='B')
+    pdf.cell( w=50,h=8,txt=f"Invoice nr. {invoice_nr}")
+    pdf.output(f"Project 5- Excel to pdf invoice\\PDFs\\{filename}.pdf")
