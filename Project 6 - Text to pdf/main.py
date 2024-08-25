86180d540cd243ec2986d555cbeaba531fb38aa5
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-08-25 15:00:01 +0530

    Content added

--- Project 6 - Text to pdf/main.py
+++ Project 6 - Text to pdf/main.py
@@ -13,4 +13,9 @@ for file in filepath:
     pdf.set_font(family="Times",size=16,style='B')
     pdf.cell( w=50,h=8,txt=name,ln=1)
 
+    with open(file, 'r') as f:
+        content=f.read()
+    pdf.set_font(family="Times",size=12)
+    pdf.multi_cell(w=0,h=10,txt=content)
+    
 pdf.output(f"Project 6 - Text to pdf\\PDFs\\output.pdf")
